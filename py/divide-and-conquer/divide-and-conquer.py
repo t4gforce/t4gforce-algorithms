18c9d4882d5e023aeac01a50365bd10a5c140ea4
t4gforce <t4gforce@gmail.com>
2020-12-08 05:27:38 +0900

    first commit

---
+++ py/divide-and-conquer/divide-and-conquer.py
@@ -0,0 +1,16 @@
+import random
+
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+
+if __name__ == "__main__":
+    print(quicksort(random.sample(list(range(0, 20)), k=20)))
