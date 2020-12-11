6f5e2e8d9aaec718f3b66b32b4e97bf887015133
t4gforce <t4gforce@gmail.com>
2020-12-11 19:58:03 +0900

    fix truth_table.h

--- cpp/recursion_bruteforce/recursion_bruteforce/truth_table.h
+++ cpp/recursion_bruteforce/recursion_bruteforce/truth_table.h
@@ -8,18 +8,21 @@ int b[N];
 
 void truth_table(int k)
 {
-	cout << "k : " << k << "\t : " << b[0] << b[1] << b[2] << endl;
+	//cout << "k : " << k << "\t : " << b[0] << b[1] << b[2] << endl;
 	if (k < N)
 	{
+		
 		b[k] = 0;
+		//cout << "up" << endl;
 		truth_table(k + 1);
 		b[k] = 1;
+		//cout << "down" << endl;
 		truth_table(k + 1);
 	}
 	else
 	{
 		for (int i = 0; i < N; i++)
-			cout << b[i] ? 'T' : 'F';
+			cout << (b[i] ? 'T' : 'F') << " ";
 		cout << (b[0] || (b[1] && b[2]) ? 'T' : 'F') << endl;
 	}
 }
