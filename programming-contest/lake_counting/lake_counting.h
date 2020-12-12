08136cbe91a5df287a885334bd606e210fc3f844
t4gforce <t4gforce@gmail.com>
2020-12-13 06:23:32 +0900

    lake counting

---
+++ programming-contest/lake_counting/lake_counting.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void dfs(int x, int y);
+int lake_counting();
---
+++ programming-contest/lake_counting/main.cpp
@@ -0,0 +1,8 @@
+﻿#include "lake_counting.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << lake_counting() << endl;
+}
